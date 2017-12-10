5b90a73ac275072c0cdedaac8c5fc5db532c18cc
Sergey Kapustin <svkapustin@gmail.com>
2017-12-10 10:05:50 -0500

    Update license

--- test/buff_test.cpp
+++ test/buff_test.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 Sergey Kapustin <boltrobotics.com>
+/* Copyright (C) 2017 Bolt Robotics <info@boltrobotics.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
