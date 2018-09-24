becd9dacce9310c9bc9afbf6f64b6adc44e6430f
sergey <boltrobotics@gmail.com>
2018-09-24 15:37:42 -0400

    Update build files (gtest, test, etc.)

---
+++ cmake/Modules/gtest.cmake
@@ -0,0 +1,19 @@
+include(project_setup)
+
+find_package(Boost REQUIRED COMPONENTS system thread)
+find_package(GTest)
+
+if (GTEST_FOUND)
+  include_directories(${GTEST_INCLUDE_DIRS})
+else ()
+  set(GTEST_HOME $ENV{GTEST_HOME})
+
+  if (NOT GTEST_HOME)
+    set(GTEST_HOME "${CMAKE_BINARY_DIR}/gtest")
+  endif ()
+
+  # Prevent overriding the parent project's compiler/linker settings on Windows
+  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+  add_project(PREFIX gtest URL "https://github.com/google/googletest.git" HOME "${GTEST_HOME}")
+endif ()
