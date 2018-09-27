25b6fb2ac0b895c5bbaae42611d6518a62512c67
sergey <boltrobotics@gmail.com>
2018-09-27 13:31:56 -0400

    Update copyright

--- include/utility/value_codec.hpp
+++ include/utility/value_codec.hpp
@@ -1,17 +1,4 @@
-/* Copyright (C) 2018 Bolt Robotics <info@boltrobotics.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/* Copyright (C) 2018 Bolt Robotics <info@boltrobotics.com> */
 
 #ifndef _btr_ValueCodec_hpp_
 #define _btr_ValueCodec_hpp_
@@ -302,7 +289,8 @@ inline void ValueCodec::fixedInt(Buff* buff, T val, bool msb)
 template<typename T, typename FloatType>
 inline void ValueCodec::encodeShiftf(Buff* buff, FloatType val, uint8_t dec_places, bool msb)
 {
-  T output = Misc::shiftfint(val, dec_places);
+  T output = 0;
+  Misc::shiftfint(val, &output, dec_places);
   fixedInt(buff, output, msb);
 }
 
