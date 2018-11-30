a2e41ea3633d0c56cd3bb66a717d282d5e72a2f2
sergey <boltrobotics@gmail.com>
2018-11-30 11:55:46 -0500

    Add spin lock

---
+++ include/utility/spin_lock.hpp
@@ -0,0 +1,58 @@
+/* Copyright (C) 2018 Bolt Robotics <info@boltrobotics.com> */
+
+#ifndef _btr_SpinLock_hpp_
+#define _btr_SpinLock_hpp_
+
+// SYSTEM INCLUDES
+#include <atomic>
+
+namespace btr
+{
+
+/**
+ * The class provide facilities to sort data.
+ *
+ * IMPORTANT: Implementation has to be compatible with AVR platform.
+ */
+class SpinLock
+{
+public:
+
+// LIFECYCLE
+
+  SpinLock() = default;
+  ~SpinLock() = default;
+
+// OPERATIONS
+
+  void lock();
+  void unlock();
+
+private:
+
+// ATTRIBUTES
+
+  std::atomic_flag lock_ = ATOMIC_FLAG_INIT;
+
+}; // class SpinLock
+
+/////////////////////////////////////////////// INLINE /////////////////////////////////////////////
+
+/////////////////////////////////////////////// PUBLIC /////////////////////////////////////////////
+
+//============================================= OPERATIONS =========================================
+
+inline void SpinLock::lock()
+{
+  while (lock_.test_and_set(std::memory_order_acquire))
+  {}
+}
+
+inline void SpinLock::unlock()
+{
+  lock_.clear(std::memory_order_release);
+}
+
+} // namespace btr
+
+#endif // _btr_SpinLock_hpp_
