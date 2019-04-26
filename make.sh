d959803af79ec3143e84605a69dbf869ff08b8f0
Sergey Kapustin <svkapustin@gmail.com>
2019-04-26 11:15:46 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,47 +1,16 @@
 #!/bin/bash
 
-# Define a directory for third-party libraries.
-if [ -z ${XTRA_HOME} ]; then
-  XTRA_HOME=${PWD}/../xtra
-fi
-
-export GTEST_HOME=${XTRA_HOME}/gtest
-export ARDUINOCMAKE_HOME=${XTRA_HOME}/arduino-cmake
-export STM32CMAKE_HOME=${XTRA_HOME}/stm32-cmake
-export LIBOPENCM3_HOME=${XTRA_HOME}/libopencm3
-export FREERTOS_HOME=${XTRA_HOME}/FreeRTOSv10.1.1
-
-X86=0
-STM32=0
-AVR=0
-ARD=0
-DEPS=0
-
 help()
 {
-  echo -e "Usage: `basename $0` [-x] [-s] [-a] [-r] [-d] [-h]"
+  echo -e "Usage: `basename $0` [-x] [-s] [-a] [-m _model_path_] [-t] [-d] [-p _projects_home_] [-h]"
   echo -e "  -x - build x86"
   echo -e "  -s - build stm32"
   echo -e "  -a - build avr"
   echo -e "  -r - build arduino"
   echo -e "  -d - pull dependencies"
+  echo -e "  -p - projects home"
   echo -e "  -h - this help"
 }
-
-while getopts "xsardh" Option
-do
-  case $Option in
-    x) X86=1;;
-    s) STM32=1;;
-    a) AVR=1;;
-    r) ARD=1;;
-    d) DEPS=1;;
-    h) help; exit 0;;
-    \?) help; exit 22;;
-  esac
-done
-
-shift $(($OPTIND - 1))
 
 # Clone repository in github or pull any changes.
 function clone_or_pull {
@@ -55,6 +24,70 @@ function clone_or_pull {
     git clone $2 ${1}
   fi
 }
+
+X86=0
+STM32=0
+AVR=0
+ARD=0
+DEPS=0
+
+while getopts "xsardh" Option
+do
+  case $Option in
+    x) X86=1;;
+    s) STM32=1;;
+    a) AVR=1;;
+    r) ARD=1;;
+    d) DEPS=1;;
+    p) PROJECTS_HOME=${OPTARG};;
+    h) help; exit 0;;
+    \?) help; exit 22;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+################################################################################
+# Dependency paths
+
+if [ -z ${PROJECTS_HOME} ]; then
+  PROJECTS_HOME="${PWD}"
+fi
+if [ -z ${XTRA_HOME} ]; then
+  XTRA_HOME=${PROJECTS_HOME}/xtra
+fi
+
+# Bolt Robotics projects
+
+if [ -z ${CMAKEHELPERS_HOME} ]; then
+  export CMAKEHELPERS_HOME=${PROJECTS_HOME}/cmake-helpers
+fi
+
+# Third-party projects
+
+if [ -z ${CTPP2_HOME} ]; then
+  export CTPP2_HOME=${XTRA_HOME}/ctpp
+fi
+if [ -z ${GTEST_HOME} ]; then
+  export GTEST_HOME=${XTRA_HOME}/gtest
+fi
+if [ -z ${SPDLOG_HOME} ]; then
+  export SPDLOG_HOME=${XTRA_HOME}/spdlog
+fi
+if [ -z ${FREERTOS_HOME} ]; then
+  export FREERTOS_HOME=${XTRA_HOME}/FreeRTOSv10.1.1
+fi
+if [ -z ${LIBOPENCM3_HOME} ]; then
+  export LIBOPENCM3_HOME=${XTRA_HOME}/libopencm3
+fi
+if [ -z ${STM32CMAKE_HOME} ]; then
+  export STM32CMAKE_HOME=${XTRA_HOME}/stm32-cmake
+fi
+if [ -z ${ARDUINOCMAKE_HOME} ]; then
+  export ARDUINOCMAKE_HOME=${XTRA_HOME}/arduino-cmake
+fi
+
+################################################################################
 
 if [ "${DEPS}" -eq 1 ]; then
   clone_or_pull "${GTEST_HOME}" "https://github.com/google/googletest.git"
