2cf385c8507b5b68d1ce864936b4867a6edf9850
Sergey Kapustin <kapucin@gmail.com>
2024-03-10 10:56:32 -0400

    Update & improve
    * Simplified make.sh. Added flags for esp32, verbose
    * Restored full README
    * Added CMakeLists.txt for esp32

--- README.md
+++ README.md
@@ -6,4 +6,104 @@ The project contains portable C++ code for
 * improving output from unit tests
 * tracking number ranges for calculating delta, mean or median
 
-Wiki: <a href="https://boltrobotics.com/projects/utility/">boltrobotics.com/projects/utility</a>
+## Table of Contents
+
+* [Build](#Build)
+* [Unit Tests](#Unit_Tests)
+* [Utility Classes](#Utility_Classes)
+  * [AvlTree](#avl_tree)
+  * [Buff](#buff)
+  * [Misc](#misc)
+  * [SharedPtr](#shared_ptr)
+  * [Sorters](#sorters)
+  * [SpinLock](#spin_lock)
+  * [TestHelpers](#test_helpers)
+  * [ValueCodec](#value_codec)
+  * [ValueTracker](#value_tracker)
+
+<a name="Build" />
+## Build
+
+Set up the build following an <a href="/projects/cmake-helpers/#example-project">example</a> from
+cmake-helpers project.
+
+<a name="Unit_Tests" />
+## Tests
+
+Build unit tests with
+```
+make.sh -x -t
+```
+
+Run the tests with
+```
+./build-x86/Release/bin/utility-tests
+```
+or
+```
+cd build-x86 && ninja test
+```
+
+<a name="Utility_Classes" />
+## Utility Classes
+
+<a name="avl_tree" />
+### <a href="include/utility/avl_tree.hpp">AvlTree</a>
+
+Implements an AVL tree. See usage examples in <a href="test/avl_tree_test.cpp">
+avl_tree_test.cpp</a>
+
+<a name="buff"></a> 
+### <a href="include/utility/buff.hpp">Buff</a>
+
+Implements dynamicaly resizable buffer. Provides read/write pointer tracking. See usage examples in
+<a href="test/buff_test.cpp">buff_test.cpp</a>
+
+<a name="misc" ></a>
+### <a href="include/utility/misc.hpp">Misc</a>
+
+Implements various utility functions such as
+* Translation between value ranges
+* Calculation of angle delta squared
+* Computing modulo with negative numbers
+* Setting/clearing bits at given byte addresses
+* Converting degrees to radians and vice versa
+* Converting binary data to human-readable hex representation
+
+See usage examples in <a href="test/misc_test.cpp">misc_test.cpp</a>
+
+<a name="shared_ptr"></a>
+### <a href="include/utility/shared_ptr.hpp">SharedPtr</a>
+
+Implements basic shared pointer to be used on an embedded platform. See usage examples in
+<a href="test/shared_ptr_test.cpp">shared_ptr_test.cpp</a>
+
+<a name="sorters"></a>
+### <a href="include/utility/sorters.hpp">Sorters</a>
+
+Implements sorting algorithms for sorting a plain-old-data array. See usage examples in
+<a href="test/sorters_test.cpp">sorters_test.cpp</a>
+
+<a name="spin_lock" ></a>
+### <a href="include/utility/spin_lock.hpp">SpinLock</a>
+
+Implements a basic spin lock for x86.
+
+<a name="test_helpers" ></a>
+### <a href="include/utility/test_helpers.hpp">TestHelpers</a>
+
+Implements useful facilities for when unit testing. See usage examples in unit tests, example: 
+<a href="test/value_codec_test.cpp">value_codec_test.cpp</a>
+
+<a name="value_codec" ></a>
+### <a href="include/utility/value_codec.hpp">ValueCodec</a>
+
+Implements number encoding and decoding facilities accounting for target host order. See usage
+examples in <a href="test/value_codec_test.cpp">value_codec_test.cpp</a>
+
+<a name="value_tracker"></a>
+### <a href="include/utility/value_tracker.hpp">ValueTracker</a>
+
+The class keeps track of numeric values so as to calculate the delta, mean or median between them.
+See usage examples in
+<a href="test/value_tracker_test.cpp">value_tracker_test.cpp</a>
--- make.sh
+++ make.sh
@@ -1,52 +1,5 @@
 #!/bin/bash
 
-help()
-{
-  echo -e "Usage: `basename $0` [-x] [-s] [-a] [-d] [-t] [-p _projects_home_] [-h]"
-  echo -e "  -x - build x86"
-  echo -e "  -s - build stm32"
-  echo -e "  -a - build avr"
-  echo -e "  -d - pull dependencies"
-  echo -e "  -t - enable unit tests"
-  echo -e "  -p - absolute path to projects home"
-  echo -e "  -h - this help"
-}
-
-# Clone repository in github or pull any changes.
-function clone_or_pull {
-  echo "Checking $(basename $1)"
-  echo -e "  Source: $2"
-  echo -e "  Target: $1"
-
-  if [ -d ${1} ]; then
-    (cd ${1} && git pull)
-  else
-    git clone $2 ${1}
-  fi
-}
-
-X86=0
-STM32=0
-AVR=0
-DEPS=0
-TESTS=""
-
-while getopts "xsadtp:h" Option
-do
-  case $Option in
-    x) X86=1;;
-    s) STM32=1;;
-    a) AVR=1;;
-    d) DEPS=1;;
-    t) TESTS="-DENABLE_TESTS=ON";;
-    p) PROJECTS_HOME=${OPTARG};;
-    h) help; exit 0;;
-    \?) help; exit 22;;
-  esac
-done
-
-shift $(($OPTIND - 1))
-
 ################################################################################
 # Dependency paths
 
@@ -58,8 +11,6 @@ if [ -z ${XTRA_HOME} ]; then
   export XTRA_HOME=${PROJECTS_HOME}/other
 fi
 
-# Bolt Robotics projects
-
 if [ -z ${CMAKEHELPERS_HOME} ]; then
   export CMAKEHELPERS_HOME=${PROJECTS_HOME}/cmake-helpers
 fi
@@ -69,49 +20,89 @@ fi
 
 # Third-party projects
 
-if [ -z ${CTPP2_HOME} ]; then
-  export CTPP2_HOME=${XTRA_HOME}/ctpp
-fi
 if [ -z ${GTEST_HOME} ]; then
   export GTEST_HOME=${XTRA_HOME}/gtest
 fi
-if [ -z ${SPDLOG_HOME} ]; then
-  export SPDLOG_HOME=${XTRA_HOME}/spdlog
-fi
 if [ -z ${FREERTOS_HOME} ]; then
-  export FREERTOS_HOME=${XTRA_HOME}/FreeRTOSv10.1.1
+  export FREERTOS_HOME=${XTRA_HOME}/freertos
 fi
 if [ -z ${LIBOPENCM3_HOME} ]; then
   export LIBOPENCM3_HOME=${XTRA_HOME}/libopencm3
 fi
 
 ################################################################################
+# Functions
 
-if [ "${DEPS}" -eq 1 ]; then
-  clone_or_pull "${GTEST_HOME}" "https://github.com/google/googletest.git"
-  clone_or_pull "${LIBOPENCM3_HOME}" "https://github.com/libopencm3/libopencm3.git"
-fi
+function clone_impl()
+{
+  echo "Checking $(basename $2)"
+  echo -e "  Source: $1"
+  echo -e "  Target: $2"
 
-if [ ${X86} -eq 1 ]; then
-  (cd ${UTILITY_HOME} \
-    && mkdir -p "build-x86" \
-    && cd "build-x86" \
-    && cmake -DBTR_X86=1 ${TESTS} "$@" .. \
-    && make)
-fi
+  if [ -d ${2} ]; then
+    (cd ${2} && git pull)
+  else
+    git clone $1 ${2}
+  fi
+}
 
-if [ ${STM32} -eq 1 ]; then
-  (cd ${UTILITY_HOME} \
-    && mkdir -p "build-stm32" \
-    && cd "build-stm32" \
-    && cmake -DBTR_STM32=1 "$@" .. \
-    && make)
-fi
+function clone()
+{
+  clone_impl "https://github.com/google/googletest.git" "${GTEST_HOME}" 
+  clone_impl "https://github.com/libopencm3/libopencm3.git" "${LIBOPENCM3_HOME}" 
+  clone_impl "https://github.com/FreeRTOS/FreeRTOS-Kernel.git" "${FREERTOS_HOME}" 
+}
 
-if [ ${AVR} -eq 1 ]; then
-  (cd ${UTILITY_HOME} \
-    && mkdir -p "build-avr" \
-    && cd "build-avr" \
-    && cmake -DBTR_AVR=1 "$@" .. \
-    && make)
-fi
+# Process command-line options
+
+x86=0; avr=0; stm32=0; esp32=0;
+TESTS=""
+VERBOSE=""
+COMPILELOG=""
+
+function build()
+{
+  (set -x && \
+    cd ${UTILITY_HOME} \
+    && mkdir -p build-${1} && cd build-${1} \
+    && cmake -G Ninja -DBTR_${1^^}=1${TESTS}${VERBOSE}${COMPILELOG}"${2}" .. \
+    && cmake --build . \
+  )
+}
+
+help()
+{
+  echo -e "Usage: `basename $0` [-x] [-a] [-s] [-e] [-d] [-c] [-v] [-t] [-h]"
+  echo -e "  -x - build x86"
+  echo -e "  -s - build stm32"
+  echo -e "  -e - build esp32"
+  echo -e "  -a - build avr"
+  echo -e "  -d - clone or pull dependencies"
+  echo -e "  -c - export compile commands"
+  echo -e "  -v - enable verbose output"
+  echo -e "  -t - enable unit tests"
+  echo -e "  -h - this help"
+}
+
+while getopts "xasedcvth" Option
+do
+  case $Option in
+    x) x86=1;;
+    a) avr=1;;
+    s) stm32=1;;
+    e) esp32=1;;
+    d) clone;;
+    c) COMPILELOG=" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON";;
+    v) VERBOSE=" -DCMAKE_VERBOSE_MAKEFILE=ON";;
+    t) TESTS=" -DENABLE_TESTS=ON";;
+    h) help; exit 0;;
+    \?) help; exit 22;;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
+if [ ${x86} -eq 1 ]; then build x86 " $@"; fi
+if [ ${avr} -eq 1 ]; then build avr " $@"; fi
+if [ ${stm32} -eq 1 ]; then build stm32 " $@"; fi
+if [ ${esp32} -eq 1 ]; then build esp32 " $@"; fi
